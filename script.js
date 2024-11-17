d4d7763b7b4601301d90f1c34fd5a2d06c44ce45
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-11-17 11:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 //your JS code here. If required.
+const output = document.getElementById('output');
+const button = document.getElementById('btn');
+
+function startPromiseChaining() {
+	button.setAttribute('disabled', true);
+	const inputNum = document.getElementById('ip').value;
+
+	const firstPromise = new Promise((resolve) => {
+		setTimeout(() => {
+			output.innerText = `Result: ${inputNum}`;
+			resolve(inputNum);
+		},2000)
+	})
+
+	firstPromise.then((res) => {
+		res *= 2;
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				output.innerText = `Result: ${res}`;
+				resolve(res);
+			}, 1000)
+		})
+	})
+	.then((res) => {
+		res -= 3;
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				output.innerText = `Result: ${res}`;
+				resolve(res);
+			}, 1000)
+		})
+	})
+	.then((res) => {
+		res /= 2;
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				output.innerText = `Result: ${res}`;
+				resolve(res);
+			}, 1000)
+		})
+	})
+	.then((res) => {
+		res += 10;
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				output.innerText = `Result: ${res}`;
+				resolve(res);
+			}, 1000)
+		})
+	})
+	.then((res) => {
+		output.innerText = `Final Result: ${res}`;
+		button.removeAttribute('disabled');
+	})
+}
